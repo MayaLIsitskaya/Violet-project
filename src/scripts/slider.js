d752f5bc451a5b95f3a4301cdd243c4a5a094eeb
Maya Lisitskaya <145857366+MayaLIsitskaya@users.noreply.github.com>
2023-12-01 21:18:58 +0300

    added header, footer and other styles and tempplates

--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -1,11 +1,8 @@
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
 
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
     direction: 'horizontal',
     loop: true,
-
-    // And if we need scrollbar
     scrollbar: {
         el: '.swiper-scrollbar',
     },
