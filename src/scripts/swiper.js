bee72b9289e9cf0deaa08f1597a09d8bf7e7be89
Maya Lisitskaya <145857366+MayaLIsitskaya@users.noreply.github.com>
2023-12-07 17:12:33 +0300

    added more styles

--- src/scripts/swiper.js
+++ src/scripts/swiper.js
@@ -1,7 +1,6 @@
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
 
 const swiper = new Swiper('.swiper', {
-    direction: 'horizontal',
     loop: true,
     scrollbar: {
         el: '.swiper-scrollbar',
